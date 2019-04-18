ae233c79f21ca1bb1971a613c653e9d2c7106d8f
B_Budowniczy <krauzik06@o2.pl>
2019-04-18 15:30:04 +0200

    formatki formatki

--- reports/report_frontpages.py
+++ reports/report_frontpages.py
@@ -14,7 +14,9 @@ from docx.enum.text import WD_TAB_ALIGNMENT, WD_TAB_LEADER
 from docx.oxml.ns import nsdecls
 from docx.oxml import parse_xml
 
-
+#standard z wykresami (nordy holandia)
+#na podstawie raportu 1969-2019 NordOrchid
+#bez PMS
 def standard(document):
 	document.add_paragraph()
 	H1 = document.add_paragraph('Measurement condition')
@@ -31,39 +33,38 @@ def standard(document):
 	T1 = document.add_paragraph()
 	r0 = T1.add_run(
 			'Measured values are presented in the table below. Each machine if applicable is separated for driver (el. motor, diesel engine, etc.) and driven unit (pump, compressor, etc.). ')
-	r1 = T1.add_run('First column ')
+	r1 = T1.add_run('First column')
 	r1.italic = True
 	r1.underline = True
-	r2 = T1.add_run('of the table consist name of the equipment. ')
-	r1 = T1.add_run('Second column ')
-	r1.italic = True
-	r1.underline = True
-	r2 = T1.add_run(
-			'contains the highest value of vibration velocity measured on the equipment in all measurement points. ')
-	r1 = T1.add_run('Third column ')
+	r2 = T1.add_run(' of the table consist name of the equipment. ')
+	r1 = T1.add_run('Second column')
 	r1.italic = True
 	r1.underline = True
 	r2 = T1.add_run(
-			'contains classification of the vibration class according to proper ISO standard and other normative documents. Classification depends on highest reading of measured equipment only. ')
-	r1 = T1.add_run('Fourth column ')
+			' contains the highest value of vibration velocity measured on the equipment in all measurement points. ')
+	r1 = T1.add_run('Third column')
 	r1.italic = True
 	r1.underline = True
 	r2 = T1.add_run(
-			'contains additional readings of enveloped value of acceleration, which is helpful in detection of early stage of bearing wear. ')
-	r1 = T1.add_run('Fifth column ')
-	r1.italic = True
-	r1.underline = True
-	r2 = T1.add_run('contains vibration trend values if previous results are available from the same source. ')
-	r1 = T1.add_run('Sixth column  ')
+			' contains classification of the vibration class according to proper ISO standard and other normative documents. Classification depends on highest reading of measured equipment only. ')
+	r1 = T1.add_run('Fourth column')
 	r1.italic = True
 	r1.underline = True
 	r2 = T1.add_run(
-			'contains remarks and suggestions based on the analysis of vibration signal. This column can be taken as the final conclusion about machine condition. If cell is empty, it means that there is no existing problem or defect shown in vibration signal.')
+			' contains additional readings of enveloped value of acceleration, which is helpful in detection of early stage of bearing wear. ')
+	r1 = T1.add_run('Fifth column')
+	r1.italic = True
+	r1.underline = True
+	r2 = T1.add_run(' contains vibration trend values if previous results are available from the same source. ')
+	r1 = T1.add_run('Sixth column')
+	r1.italic = True
+	r1.underline = True
+	r2 = T1.add_run(
+			' contains remarks and suggestions based on the analysis of vibration signal. This column can be taken as the final conclusion about machine condition. If cell is empty, it means that there is no existing problem or defect shown in vibration signal.')
 	T1.style = document.styles['IM TEXT']
 	T1.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
-
-#donnelly
-def standard_with_pms_charts(document):
+#j/w + pms
+def standard_with_pms(document):
 	#enter po tabeli
 	document.add_paragraph()
 	# header nr 1
@@ -90,7 +91,7 @@ def standard_with_pms_charts(document):
 
 	r2.italic=True
 	r2.underline=True
-	r2=h3.add_run(' of the table consist PMS number and name of the equipment. ')
+	r2=h3.add_run(' of the table consist PMS number and name of the equipment.')
 	r3=h3.add_run('Third column')
 	r3.italic=True
 	r3.underline=True
@@ -111,40 +112,91 @@ def standard_with_pms_charts(document):
 	r11.underline=True
 	r11.italic=True
 	r12=h3.add_run(' contains remarks and suggestions based on the analysis of vibration signal. This column can be taken as the final conclusion about machine condition. If cell is empty, it means that there is no existing problem or defect shown in vibration signal.')
-#bourbony
-def standard_with_charts(document):
-
+	r12.add_break()
+#siem z limitem bez PMS
+def standard_non_PMS_limit(document):
 	document.add_paragraph()
-	H1=document.add_paragraph('Measurement condition')
+	H0=document.add_paragraph('Measurement condition')
+	H0.style=document.styles['IM HEAD']
+	document.add_paragraph('Measurements were taken during normal condition.')
+	document.add_paragraph()
+	H1=document.add_paragraph('Results presentation')
 	H1.style=document.styles['IM HEAD']
-	p1=document.add_paragraph("Measurements were taken during normal operating condition.")
-	document.add_paragraph()
-	H2=document.add_paragraph('Results presentation').style=document.styles['IM HEAD']
-	p2=document.add_paragraph()
-	r0=p2.add_run('Measured values are presented in the table below. Each machine if applicable is separated for driver (el. motor, diesel engine, etc.) and driven unit (pump, compressor, etc.). ')
-	r0.add_break()
-	r1=p2.add_run("First column")
+	p1=document.add_paragraph()
+	r0=p1.add_run("Measured values are presented in the table below. Each machine if applicable is separated for driver (el. motor, diesel engine, etc.) and driven unit (pump, compressor, etc.). ")
+	r1=p1.add_run('First column')
 	r1.underline=True
 	r1.italic=True
-	r2=p2.add_run(' of the table consist name of the equipment. ')
-	r3=p2.add_run('Second column')
+	r2=p1.add_run(' of the table consist PMS number and name of the equipment. ')
+	r3=p1.add_run('Second column')
 	r3.underline=True
 	r3.italic=True
-	r4=p2.add_run(' contains the highest value of vibration velocity measured on the equipment in all measurement points. ')
-	r5=p2.add_run('Third column')
+	r4=p1.add_run(' contains the highest value of vibration velocity measured on the equipment in all measurement points. ')
+	r5=p1.add_run('Third column')
 	r5.italic=True
 	r5.underline=True
-	r6=p2.add_run(' contains classification of the vibration class according to proper ISO standard and other normative documents. Classification depends on highest reading of measured equipment only. ')
-	r7=p2.add_run('Fourth column')
-	r7.italic=True
+	r6=p1.add_run(' contains ISO classification limit. ')
+	r7=p1.add_run('Fourth column')
 	r7.underline=True
-	r8=p2.add_run(' contains additional readings of enveloped value of acceleration, which is helpful in detection of early stage of bearing wear. ')
-	r9=p2.add_run('Fifth column')
-	r9.underline=True
+	r7.italic=True
+	r8=p1.add_run(' contains classification of the vibration class according to proper ISO standard and other normative documents. Classification depends on highest reading of measured equipment only. ')
+	r9=p1.add_run('Fifth column')
 	r9.italic=True
-	r10=p2.add_run(' contains vibration trend values if previous results are available from the same source. ')
-	r11=p2.add_run('Sixth column')
-	r11.italic=True
+	r9.underline=True
+	r10=p1.add_run(' contains additional readings of enveloped value of acceleration, which is helpful in detection of early stage of bearing wear. ')
+	r11=p1.add_run('Sixth column')
 	r11.underline=True
-	r12=p2.add_run(' contains remarks and suggestions based on the analysis of vibration signal. This column can be taken as the final conclusion about machine condition. If cell is empty, it means that there is no existing problem or defect shown in vibration signal.')
-	r12.add_break()
+	r11.italic=True
+	r12=p1.add_run(' contains vibration trend values if previous results are available from the same source. ')
+	r13=p1.add_run('Seventh column')
+	r13.italic=True
+	r13.underline=True
+	p1.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+	r14=p1.add_run(' contains remarks and suggestions based on the analysis of vibration signal. This column can be taken as the final conclusion about machine condition. If cell is empty, it means that there is no existing problem or defect shown in vibration signal.')
+	r14.add_break()
+#siem z PMS i LIMITEM
+#poniżej improwizacja - nie znalazłem raportu dla jakiegokolwiek siema z PMS
+def standard_PMS_limit(document):
+	document.add_paragraph()
+	H0=document.add_paragraph("Measurement condition")
+	H0.style=document.styles['IM HEAD']
+	document.add_paragraph('Measurements were taken during normal condition.')
+	document.add_paragraph()
+
+
+	H1=document.add_paragraph('Results presentation')
+	H1.style=document.styles['IM HEAD']
+	document.add_paragraph('Measured values are presented in the table below. Each machine if applicable is separated for driver (el. motor, diesel engine, etc.) and driven unit (pump, compressor, etc.). ')
+	p1=document.add_paragraph()
+	r1=p1.add_run('First and second column')
+	r1.italic=True
+	r1.underline=True
+	r2=p1.add_run(' of the table consist PMS number and name of the equipment. ')
+	r3=p1.add_run('Third column')
+	r3.underline=True
+	r3.italic=True
+	r4=p1.add_run(' contains ISO classification limit. ')
+	r5=p1.add_run('Fourth column')
+	r5.underline=True
+	r5.italic=True
+	r6=p1.add_run(' contains classification of the vibration class according to proper ISO standard and other normative documents. Classification depends on highest reading of measured equipment only. ')
+	r7=p1.add_run('Fifth column')
+	r7.underline=True
+	r7.italic=True
+	r8=p1.add_run(' contains additional readings of enveloped value of acceleration, which is helpful in detection of early stage of bearing wear. ')
+	r9=p1.add_run('Sixth column')
+	r9.italic=True
+	r9.underline=True
+	r10=p1.add_run(' contains vibration trend values if previous results are available from the same source. ')
+	r11=p1.add_run('Seventh column')
+	r11.underline=True
+	r11.italic=True
+	r12=p1.add_run(' contains remarks and suggestions based on the analysis of vibration signal. This column can be taken as the final conclusion about machine condition. If cell is empty, it means that there is no existing problem or defect shown in vibration signal.')
+	p1.alignment=WD_ALIGN_PARAGRAPH.JUSTIFY
+
+
+
+
+
+
+
